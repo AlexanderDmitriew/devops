913614b5ae6810c1eabb8bbcb4e6bac589a55a31
Alexander_Dmitriev <alex4056546@yandex.ru>
2024-03-31 17:59:51 +1000

    fix variables file

--- infrastructure/modules/instance/variables.tf
+++ infrastructure/modules/instance/variables.tf
@@ -1,54 +1,44 @@
-# Подключемся через Terraform к сервисной srv ноде с использованием ключа.
-resource "null_resource" "srv" {
-  depends_on = [yandex_compute_instance.srv]
-  connection {
-    user        = var.ssh_credentials.user
-    private_key = file(var.ssh_credentials.private_key)
-    host        = yandex_compute_instance.srv.network_interface.0.nat_ip_address
-  }
+# Переменная определяющая зону для разворачивания ВМ
+variable "zone" {                                                   # Используем переменную для передачи в конфиг инфраструктуры
+  description = "Use specific availability zone"                    # Опционально описание переменной
+  type        = list(string)                                        # Опционально тип переменной, коллекция последовательностей значения
+  default     = ["ru-central1-a", "ru-central1-b", "ru-central1-c"] # Опционально значение переменной зоны расположения сервера, перечисленные по порядку для выбора
+}
 
+# Указываем какой имидж ОС использовать
+variable "instance_family_image" {
+  description = "Instance image"
+  type        = string
+  default     = "ubuntu-2204-lts"
+}
 
- # Копируем в srv ноду для установки бинарные файлы terraform и terragrunt по относительному пути от папки проекта 
- # https://developer.hashicorp.com/terraform/language/resources/provisioners/file
-  provisioner "file" {
-    source      = "soft/"
-    destination = "/home/ubuntu"
-  }
+variable "vpc_subnet_id" {
+  description = "VPC subnet network id"
+  type        = string
+}
 
-# Копируем в srv ноду файл для установки конфига с настройкой зеркала yandex для продуктов hashicorp
-  provisioner "file" {
-    source      = "configs/"
-    destination = "/home/ubuntu"
-  }
-
-# Копируем в srv ноду скрипт для установки gitlab-runner, kubectl, kubeadm, helm 
-  provisioner "file" {
-    source      = "scripts/"
-    destination = "/home/ubuntu"
-  }
-
-# Копируем в srv ноду ключи 
-  provisioner "file" {
-    source      = "key/"
-    destination = "/home/ubuntu"
-  }
-
-# Копируем в srv ноду var креды для подключения к яндекс облаку с неё
-provisioner "file" {
-    source      = "modules/instance/private.variables.tf"
-    destination = "/home/ubuntu/private.variables.tf"
-  }
-  
-
-# Устанавливаем на сервисную srv ноду terraform - делаем исполняемыми скопированные бинарные файлы. Делаем исполняемым скрипт.
-# Устанавливаем на сервисную srv ноду git, gitlab-runner, ansible, jq, docker, kubeadm, kubectl, gitlab-runner.
-# https://developer.hashicorp.com/terraform/language/resources/provisioners/remote-exec
-  provisioner "remote-exec" {
-    inline = [
-      "sudo chmod +x /home/ubuntu/install.sh",
-      "sudo /home/ubuntu/install.sh",        
-      "sleep 25",
-      "echo COMPLETED_install" 
-    ]
+# Указываем где и какой ключ взять
+variable "ssh_credentials" {
+  description = "Credentials for connect to instances"
+  type        = object({
+    user        = string
+    private_key = string
+    pub_key     = string
+  })
+  default     = {
+    user        = "ubuntu"
+    private_key = "~/.ssh/id_rsa"
+    pub_key     = "~/.ssh/id_rsa.pub"
   }
 }
+variable "user_token" {
+  type = string
+}
+
+variable "user_cloud" {
+  type = string
+}
+variable "user_folder" {
+  type = string
+}
+  
